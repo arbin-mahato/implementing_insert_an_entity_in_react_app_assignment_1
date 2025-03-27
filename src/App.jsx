b2d328b4047eb0cb35069e48b7ec9148f5269e7a
arbin-mahato <proarbind@gmail.com>
2025-03-27 11:23:33 +0530

    Add book button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 // src/App.jsx
-
-import React from 'react';
-import Home from './Home';
-import './App.css'; // Import global styles
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import AddBookForm from "./AddBookForm";
+import "./App.css"; // Import global styles
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add" element={<AddBookForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
